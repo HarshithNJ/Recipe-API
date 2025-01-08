2ab83ac0399303f965ad7c2cf9262a8fac8d1796
Harshith N J <njharshith122@gmail.com>
2025-01-08 12:25:08 +0530

    create statement completed

--- src/main/java/org/recipe/recipe_api/controller/recipeController.java
+++ src/main/java/org/recipe/recipe_api/controller/recipeController.java
@@ -1,10 +1,15 @@
 package org.recipe.recipe_api.controller;
 
+import org.recipe.recipe_api.dto.recipe;
 import org.recipe.recipe_api.service.recipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -13,5 +18,12 @@ public class recipeController {
 
     @Autowired
     recipeService service;
+
+    /*To Save the record*/
+    @PostMapping("/recipes")
+    public ResponseEntity<Object> saveRecipe(@RequestBody recipe recipe) {
+        return service.saveRecipe(recipe);
+    }
+    
     
 }
--- src/main/java/org/recipe/recipe_api/service/recipeService.java
+++ src/main/java/org/recipe/recipe_api/service/recipeService.java
@@ -1,7 +1,13 @@
 package org.recipe.recipe_api.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.recipe.recipe_api.dto.recipe;
 import org.recipe.recipe_api.repository.recipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +15,21 @@ public class recipeService {
     
     @Autowired
     recipeRepository repository;
+
+    public ResponseEntity<Object> saveRecipe(recipe recipe) {
+        if(repository.existByName(recipe.getName())){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Recipe name Alredy Exist");
+
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_ACCEPTABLE);
+        }else{
+            repository.save(recipe);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Record successfully created");
+            map.put("Data", recipe);
+            
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
 }
